2220acf12a6351ca5175a9fcb1a717cea49dd0c0
Michael Perez <mperez980089@yahoo.com>
2018-10-14 03:14:01 -0700

    Added yelling

--- Assets/Scripts/Superintendent.cs
+++ Assets/Scripts/Superintendent.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Superintendent : MonoBehaviour
 {
+    public AudioClip[] yells;
+    public float nextTalkTimerMin;
+    public float nextTalkTimerMax;
+    float nextTalkTimer;
+
+    AudioSource audioSource;
+
     void Start()
     {
-
+        nextTalkTimer = Random.Range(nextTalkTimerMin, nextTalkTimerMax);
     }
 
     void Update()
     {
-
+        nextTalkTimer -= Time.deltaTime;
+        if (nextTalkTimer <= 0)
+        {
+            nextTalkTimer = Random.Range(nextTalkTimerMin, nextTalkTimerMax);
+            audioSource.clip = yells[Random.Range(0, yells.Length - 1)];
+            audioSource.Play();
+        }
     }
 }
